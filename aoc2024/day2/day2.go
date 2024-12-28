3261373b1c5f498eeb6e5344afda44115e7cb17a
Kate Shaffer <kate.shaffer@gmail.com>
2024-12-28 04:20:08 -0800

    More WIP Day 1, part 2 (works with mess)

--- aoc2024/day2/day2.go
+++ aoc2024/day2/day2.go
@@ -2,19 +2,18 @@ package day2
 
 import (
 	"aoc2024/util"
-	"fmt"
 	"slices"
 )
 
 func Day2() {
 	//safeReportTotal := countSafeReports(getInput("day2/sample"))
-	safeReportTotal := countSafeReports(getInput("day2/input"))
+	//safeReportTotal := countSafeReports(getInput("day2/input"))
 
 	//safeWithToleranceReportTotal := countSafeReportsWithTolerance(getInput("day2/sample"), 1)
-	//safeWithToleranceReportTotal := countSafeReportsWithTolerance(getInput("day2/input"), 1)
-	safeWithToleranceReportTotal := countSafeReportsWithTolerance(getInput("day2/edgeCasesSample"), 1) // input from: https://www.reddit.com/r/adventofcode/comments/1h4shdu/2024_day_2_part2_edge_case_finder/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+	safeWithToleranceReportTotal := countSafeReportsWithTolerance(getInput("day2/input"), 1)
+	//safeWithToleranceReportTotal := countSafeReportsWithTolerance(getInput("day2/edgeCasesSample"), 1) // input from: https://www.reddit.com/r/adventofcode/comments/1h4shdu/2024_day_2_part2_edge_case_finder/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 
-	println(">>>>> TOTAL SAFE WITHOUT TOLERANCE: %v", safeReportTotal)
+	//println(">>>>> TOTAL SAFE WITHOUT TOLERANCE: %v", safeReportTotal)
 	println(">>>>> TOTAL SAFE WITH TOLERANCE: %v", safeWithToleranceReportTotal)
 }
 
@@ -42,40 +41,39 @@ func countSafeReportsWithTolerance(reports [][]int, toleranceCount int) int {
 	originalTolerance := toleranceCount
 
 	for reportIndex, report := range reports {
-		if report[1] < report[0] {
-			// decreasing. reverse, to pass into below block
-			slices.Reverse(report)
-		}
-
-		if safe, indexFoundUnsafe := isSafe(report); safe {
-			fmt.Printf("found safe reportIndex: %v, report values: %v\n", reportIndex, report)
+		if safe, _ := isSafe(report); safe {
+			//fmt.Printf("found safe reportIndex: %v, report values: %v\n", reportIndex, report)
 			count++
 		} else {
-			fmt.Printf("found unsafe reportIndex: %v, report values: %v\n", reportIndex, report)
+			//fmt.Printf("found unsafe reportIndex: %v, report values: %v\n", reportIndex, report)
 			if toleranceCount > 0 {
 				toleranceCount--
-				reportWithHigherIndexRemoved := slices.Concat(report[:indexFoundUnsafe], report[indexFoundUnsafe+1:])
-				reportWithLowerIndexRemoved := slices.Concat(report[:indexFoundUnsafe-1], report[indexFoundUnsafe:])
-				countMaybeSaferHigherIndexRemoved := countSafeReportsWithTolerance([][]int{reportWithHigherIndexRemoved}, toleranceCount)
-				countMaybeSaferLowerIndexRemoved := countSafeReportsWithTolerance([][]int{reportWithLowerIndexRemoved}, toleranceCount)
-				if countMaybeSaferHigherIndexRemoved > 0 || countMaybeSaferLowerIndexRemoved > 0 {
-					count += max(countMaybeSaferHigherIndexRemoved, countMaybeSaferLowerIndexRemoved)
+
+				for i := range report {
+					retriedReport := slices.Delete(slices.Clone(report), i, i+1)
+					if retriedSafe, _ := isSafe(retriedReport); retriedSafe {
+						//fmt.Printf("found safe on retry reportIndex: %v, report values: %v\n", reportIndex, retriedReport)
+						count++
+						break
+					}
 				}
+
+				toleranceCount = originalTolerance
 			}
 		}
-		toleranceCount = originalTolerance
 	}
 	return count
 }
 
 func isSafe(report []int) (bool, int) {
+	reportToEvaluate := slices.Clone(report)
 	if report[1] < report[0] {
 		// decreasing. reverse, to pass into below block
-		slices.Reverse(report)
+		slices.Reverse(reportToEvaluate)
 	}
 
-	for i := 1; i < len(report); i++ {
-		diff := report[i] - report[i-1]
+	for i := 1; i < len(reportToEvaluate); i++ {
+		diff := reportToEvaluate[i] - reportToEvaluate[i-1]
 		if diff < 1 || diff > 3 {
 			return false, i
 		}
@@ -83,13 +81,70 @@ func isSafe(report []int) (bool, int) {
 	return true, -1
 }
 
-func getSampleInputReports() [][]int {
-	return [][]int{
-		{7, 6, 4, 2, 1},
-		{1, 2, 7, 8, 9},
-		{9, 7, 6, 2, 1},
-		{1, 3, 2, 4, 5},
-		{8, 6, 4, 4, 1},
-		{1, 3, 6, 7, 9},
-	}
-}
+// TODO: remove – too complicated & cute / didn't work
+//func countSafeReportsWithTolerance(reports [][]int, toleranceCount int) int {
+//	count := 0
+//	originalTolerance := toleranceCount
+//
+//	for reportIndex, report := range reports {
+//		if report[1] < report[0] {
+//			// decreasing. reverse, to pass into below block
+//			slices.Reverse(report)
+//		}
+//
+//		if safe, indexFoundUnsafe := isSafe(report); safe {
+//			fmt.Printf("found safe reportIndex: %v, report values: %v\n", reportIndex, report)
+//			count++
+//		} else {
+//			fmt.Printf("found unsafe reportIndex: %v, report values: %v\n", reportIndex, report)
+//			if toleranceCount > 0 {
+//				toleranceCount--
+//				reportWithHigherIndexRemoved := slices.Concat(report[:indexFoundUnsafe], report[indexFoundUnsafe+1:])
+//				reportWithLowerIndexRemoved := slices.Concat(report[:indexFoundUnsafe-1], report[indexFoundUnsafe:])
+//				countMaybeSaferHigherIndexRemoved := countSafeReportsWithTolerance([][]int{reportWithHigherIndexRemoved}, toleranceCount)
+//				countMaybeSaferLowerIndexRemoved := countSafeReportsWithTolerance([][]int{reportWithLowerIndexRemoved}, toleranceCount)
+//				if countMaybeSaferHigherIndexRemoved > 0 || countMaybeSaferLowerIndexRemoved > 0 {
+//					count += max(countMaybeSaferHigherIndexRemoved, countMaybeSaferLowerIndexRemoved)
+//				}
+//			}
+//		}
+//		toleranceCount = originalTolerance
+//	}
+//	return count
+//}
+
+// TODO: remove – this one was REALLY a mess...stack overflowed?
+//func countSafeReportsWithTolerance(reports [][]int, toleranceCount int) int {
+//	count := 0
+//	originalTolerance := toleranceCount
+//
+//	for reportIndex, report := range reports {
+//		//if report[1] < report[0] {
+//		//	// decreasing. reverse, to pass into below block
+//		//	slices.Reverse(report)
+//		//}
+//		reportReversed := slices.Clone(report)
+//		slices.Reverse(reportReversed)
+//
+//		if safe, indexFoundUnsafe := isSafe(report); safe {
+//			count++
+//		} else if countMaybeSaferReversed := countSafeReportsWithTolerance([][]int{reportReversed}, toleranceCount); countMaybeSaferReversed > 0 {
+//			count += countMaybeSaferReversed
+//		} else {
+//			fmt.Printf("found unsafe reportIndex: %v, report values: %v\n", reportIndex, report)
+//			if toleranceCount > 0 {
+//				toleranceCount--
+//				reportWithHigherIndexRemoved := slices.Concat(report[:indexFoundUnsafe], report[indexFoundUnsafe+1:])
+//				reportWithLowerIndexRemoved := slices.Concat(report[:indexFoundUnsafe-1], report[indexFoundUnsafe:])
+//
+//				countMaybeSaferHigherIndexRemoved := countSafeReportsWithTolerance([][]int{reportWithHigherIndexRemoved}, toleranceCount)
+//				countMaybeSaferLowerIndexRemoved := countSafeReportsWithTolerance([][]int{reportWithLowerIndexRemoved}, toleranceCount)
+//				if countMaybeSaferHigherIndexRemoved > 0 || countMaybeSaferLowerIndexRemoved > 0 {
+//					count += max(countMaybeSaferHigherIndexRemoved, countMaybeSaferLowerIndexRemoved)
+//				}
+//			}
+//		}
+//		toleranceCount = originalTolerance
+//	}
+//	return count
+//}
